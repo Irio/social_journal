9b143833b5e39f116ce5caa0d8e74df1824ee176
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-06-03 22:33:48 -0300

    Render list of posts in Home

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,4 +6,9 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should assign a collection of posts" do
+    Post.create!(title: 'My first post', content: 'Lorem ipsum')
+    get :home
+    assert_kind_of Post, assigns(:posts).first
+  end
 end
