c5f671171f16f2a9bcc690fd574fb9cf07f6280a
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-06-03 21:02:59 -0300

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialJournal::Application.config.secret_key_base = '52482ece5fc1754174232ae2c3cee86d9eb2d48fa43bc7e8f435ccbb7e7189e946c2e90a0d20ab5cf62f56935530ca9e6e41abbdae07ad314352d1c8b5edb8e3'
