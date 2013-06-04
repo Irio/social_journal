29450da050297f5d8da088e4eca6f4d58733e6b3
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-06-03 21:41:06 -0300

    Import Twitter Bootstrap and create a template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @posts = [
+      { title: 'Diminuição do preço da passagem', content: 'EU VI mais de 5 mil pessoas que não paravam de gritar e cantar se botarem em silêncio para passar em frente a um hospital. EU VI dois caras derrubarem um contêiner de lixo e imediatamente uns dez outros manifestantes correrem pra levantar o contêiner.
+Hoje eu vi um protesto lindo, organizado, forte, inesquecível, e tenho certeza que não é do que vi que a velha mídia vai falar. Mas eu vou gritar bem alto. E mais milhares também vão.' },
+      { title: 'Diminuição do preço da passagem', content: 'EU VI mais de 5 mil pessoas que não paravam de gritar e cantar se botarem em silêncio para passar em frente a um hospital. EU VI dois caras derrubarem um contêiner de lixo e imediatamente uns dez outros manifestantes correrem pra levantar o contêiner.
+Hoje eu vi um protesto lindo, organizado, forte, inesquecível, e tenho certeza que não é do que vi que a velha mídia vai falar. Mas eu vou gritar bem alto. E mais milhares também vão.' }
+    ]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 SocialJournal::Application.routes.draw do
   root to: 'pages#home'
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
